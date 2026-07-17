acfd364d331312702e993d17c2a153a2597bf4e6
Aditi-coding29 <aditiverma567897@gmail.com>
2026-07-17 15:50:14 +0530

    python

---
+++ abc/listloop.py
@@ -0,0 +1,35 @@
+#l = [1,3,44,"aditi",56.57,True]
+#l1_num = []
+#l2_str = []
+#l3_float = []
+#l4_bool = []
+#for i in l:
+    #if type(i) == int:
+       # l1_num.append(i)
+    #elif type(i) == str:
+     #   l2_str.append(i)
+    #elif type(i) == bool:
+   #      l4_bool.append(i)
+  #  else:
+ #       l3_float.append(i)
+#print("float",l3_float)                
+#print("numbers",l1_num)        
+#print("strings",l2_str)
+#print("boolean",l4_bool)
+#num = int(input("enter the number:"))
+#temp = num
+#reverse = 0
+#while num>0:
+   # digit = num%10
+  #  reverse = (reverse*10)+digit
+ #   num = num//10
+#if temp == reverse:
+ #    print("number is palindrome")     
+#else:
+     #print("number is not palindrome")
+#for i in l:
+    #if i%2==0:
+       # print(i)
+    
+
+  
